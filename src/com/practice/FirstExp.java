2ac365c7d14f2be0adb4b6546df4ee0a6daaecf8
arr2311 <heyram94@gmail.com>
2024-01-18 15:32:48 +0530

    Second

--- src/com/practice/FirstExp.java
+++ src/com/practice/FirstExp.java
@@ -2,8 +2,12 @@ package com.practice;
 
 public class FirstExp {
 	public static void main(String[] args) {
+		exp();
+	}
+	public static void exp() {
 		String a = "This sentence is for testing purpose";
 		System.out.println(a);
+		
 	}
 
 }
