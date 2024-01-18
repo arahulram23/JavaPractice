4de702627488555926a8501e7097d83fd8c51f62
arr2311 <heyram94@gmail.com>
2024-01-18 12:25:31 +0530

    Added File

---
+++ src/com/practice/RotateArray.java
@@ -0,0 +1,28 @@
+package com.practice;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+	public static void main(String[] args) {
+		
+			
+			int []nums= {1,2,3,4,5,6,7};
+			int k =3;
+			if (k > nums.length)
+				k = k % nums.length;
+			int[] result = new int[nums.length];
+			for (int i = 0; i < k; i++) {
+				result[i] = nums[nums.length - k + i];
+			}
+			int j = 0;
+			for (int i = k; i < nums.length; i++) {
+				result[i] = nums[j];
+				j++;
+			}
+			System.arraycopy(result, 0, nums, 0, nums.length);
+			System.out.println(Arrays.toString(result));
+		
+	}
+
+}
